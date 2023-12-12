2e6fef2af231d1375821e6ef3010274568a34cc2
Gabriel <podryto@protonmail.com>
2023-12-11 23:49:31 -0300

    Fim da implementacao dos endpoints relacionados a Customers.

--- src/main/kotlin/org/nothing/credit/application/system/controller/CustomerController.kt
+++ src/main/kotlin/org/nothing/credit/application/system/controller/CustomerController.kt
@@ -2,6 +2,7 @@ package org.nothing.credit.application.system.controller
 
 import org.nothing.credit.application.system.domain.Customer
 import org.nothing.credit.application.system.dto.CustomerDto
+import org.nothing.credit.application.system.dto.CustomerUpdateDto
 import org.nothing.credit.application.system.dto.CustomerView
 import org.nothing.credit.application.system.service.impl.CustomerService
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @RestController
 @RequestMapping("/api/customers")
@@ -27,7 +30,7 @@ class CustomerController(
 
     @GetMapping("/{id}")
     fun findById(@PathVariable id: Long): CustomerView {
-        val customer: Customer = this.customerService.findById(id)
+        val customer = this.customerService.findById(id)
 
         return CustomerView(customer)
     }
@@ -35,5 +38,16 @@ class CustomerController(
     @DeleteMapping("/{id}")
     fun deleteCustomer(@PathVariable id: Long) {
         this.customerService.delete(id)
+    }
+
+    @PatchMapping
+    fun updateCustomer(
+        @RequestParam(value = "customerId") id: Long, @RequestBody customerUpdateDto: CustomerUpdateDto
+    ): CustomerView {
+        val customer = this.customerService.findById(id)
+        val customerToUpdate = customerUpdateDto.updateDomainEntity(customer)
+        val customerUpdated = this.customerService.save(customerToUpdate)
+
+        return CustomerView(customerUpdated)
     }
 }
