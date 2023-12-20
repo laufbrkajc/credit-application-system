1a16159c4b72afadac1c18ab60435ccce13e395d
Gabriel <podryto@protonmail.com>
2023-12-20 02:43:24 -0300

    Pequena refatoracao.

--- src/main/kotlin/org/nothing/credit/application/system/dto/DateIsWithin.kt
+++ src/main/kotlin/org/nothing/credit/application/system/dto/DateIsWithin.kt
@@ -3,7 +3,7 @@ package org.nothing.credit.application.system.dto
 import jakarta.validation.Constraint
 import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
-import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.PositiveOrZero
 import org.nothing.credit.application.system.dto.DateIsWithin.DateIsBeforeValidator
 import java.time.LocalDate
 import kotlin.reflect.KClass
@@ -26,9 +26,9 @@ annotation class DateIsWithin(
     val message: String = "Date is out of range.",
     val groups: Array<KClass<*>> = [],
     val payload: Array<KClass<out Any>> = [],
-    @Min(value = 0) val years: Int = 0,
-    @Min(value = 0) val months: Int = 0,
-    @Min(value = 0) val days: Int = 0
+    @PositiveOrZero val years: Int = 0,
+    @PositiveOrZero val months: Int = 0,
+    @PositiveOrZero val days: Int = 0
 ) {
     class DateIsBeforeValidator : ConstraintValidator<DateIsWithin, LocalDate> {
         private var years: Long = 0
